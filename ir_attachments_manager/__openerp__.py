b2b1f6291496e7f2b34ca8029ac8b9408fa54bda
Valentin Thirion <vt@abakusitsolutions.eu>
2017-03-03 10:47:12 +0100

    [ADD] ir_attachments_manager First base

---
+++ ir_attachments_manager/__openerp__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Attachments Views",
+    'version': '9.0.1.0.1',
+    'depends': [
+        'base'
+    ],
+    'author': "Valentin Thirion, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Base',
+    'description': """
+    Attachments Views
+
+    This modules adds a menuitem in the main with some subsequent views for the attachments that are linked to models in Odoo.
+
+    This module has been developed by Valentin Thirion @ AbAKUS it-solutions.
+    """,
+    'data': ['hr_expense_view.xml',],
+    'demo': [],
+}
