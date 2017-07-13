9e314ef8cc128d46ad3b65bff9310e9ad50e633d
Valentin Thirion <vt@abakusitsolutions.eu>
2017-07-13 15:52:51 +0200

    [ADD] bac_main_template_changes Added module to repo

---
+++ bac_main_template_changes/README.md
@@ -0,0 +1,2 @@
+Odoo main template changes on demands for La Boite à Com
+This module has been developed by Valentin Thirion @ AbAKUS it-solutions.
---
+++ bac_main_template_changes/__openerp__.py
@@ -0,0 +1,15 @@
+{
+   'name': "BàC Main Template Changes",
+    'version': '9.0.1.0',
+    'depends': [],
+    'author': "Valentin Thirion, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'CSS',
+    'description': 
+    """
+    Odoo main template changes on demands for La Boite à Com
+    This module has been developed by Valentin Thirion @ AbAKUS it-solutions.
+    """,
+    'data': ['view/template.xml'],
+    'qweb': [],
+}
